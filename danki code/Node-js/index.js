c0ca2299c23edfe04a8d829bb1501e2c163a16ab
welik_carlos <frontendwelikcarlos@gmail.com>
2026-02-12 20:46:36 -0300

    node esta indo ok

--- danki code/Node-js/index.js
+++ danki code/Node-js/index.js
@@ -1,5 +1,29 @@
-function somar(n1,n2){
-    console.log(n1+n2);
-}
 
-somar(10,20);
+const http = require('http');
+const fs = require('fs');
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer(function(req,res){
+    
+    if(req.url == '/nodejs'){
+
+        fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(data);
+        return res.end();
+
+   });
+
+    }else{
+        return res.end("404 not found")
+    }
+
+
+});
+
+
+server.listen(port,hostname,()=>{
+    console.log('Servidor esta rodando ');
+})
