ae9f64f4d4488d9354e3314c8beedd6e3ce1ba53
welik <frontendwelikcarlos@gmail.com>
2026-04-30 18:10:59 -0300

    ok

--- CURSOS UDEMY/CURSO - UD - FRONT - END WEB/curso de Front end/JS - Estudos/treinando js/calculadoraMediaArray.js
+++
@@ -1,9 +0,0 @@
-const frutas = ["morango","abacaxi","banana"];
-
-frutas[1] = "não é uma fruta"// mudar
-
-frutas.push("laranja");//adicionar
-
-console.log(frutas.length)
-
-console.log(frutas[0]);
