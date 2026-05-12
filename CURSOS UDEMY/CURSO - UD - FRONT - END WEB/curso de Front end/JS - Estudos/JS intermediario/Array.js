58a75486c1c1e11bd7872df04ac57505ad48d341
welik <frontendwelikcarlos@gmail.com>
2026-05-12 20:12:43 -0300

    Iniciando o DOM

---
+++ CURSOS UDEMY/CURSO - UD - FRONT - END WEB/curso de Front end/JS - Estudos/JS intermediario/Array.js
@@ -0,0 +1,2 @@
+const lista = []
+const lista2 = new Array()// outra forma de criar uma array
---
+++ CURSOS UDEMY/CURSO - UD - FRONT - END WEB/curso de Front end/JS - Estudos/JS intermediario/funcaoNativaMatematica.js
@@ -0,0 +1,8 @@
+
+let numero = 180.50;
+
+Math.ceil(numero)// arredonda para cima 
+Math.floor(numero)//arredonda para baixo
+Math.round(numero)// arredonda para o mais proximo
+Math.random(numero)// gera numero aleatorio entre 0 a 1, mas multiplicando o ele pode chgar ate mais do que 100
+Math.ceil(numero)//
